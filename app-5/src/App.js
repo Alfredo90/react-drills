f1bffb40f7729ed1f8dfcdf62ebe85c444ec2871
unknown <kmstry214@gmail.com>
2020-10-22 00:51:14 -0500

    App-5 complete

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './image'
+class App extends Component {
+  render(){
 
-function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src='https://cdn.flickeringmyth.com/wp-content/uploads/2018/11/Dragon-Ball-Super-Broly-trailer-3-screenshot-600x329.jpg'/>
     </div>
   );
 }
 
-export default App;
+}
+
+export default App
---
+++ app-5/src/image.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+
+class Image extends Component {
+    render(){
+        return(
+            <div>
+      <img src='https://cdn.flickeringmyth.com/wp-content/uploads/2018/11/Dragon-Ball-Super-Broly-trailer-3-screenshot-600x329.jpg'/>
+                
+                <div>Error 599</div>
+            </div>
+        )
+    }
+}
+
+export default Image
